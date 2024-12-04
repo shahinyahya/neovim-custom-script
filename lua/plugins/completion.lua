462825cc5fc4eb7f88e9d635710880a7ce8de5ad
shahinyahya <shahintyahya@gmail.com>
2024-12-05 02:03:56 +0530

    confuring python snippets

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,7 +8,10 @@ return {
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
       "rafamadriz/friendly-snippets",
-    }
+    },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
   },
   {
     "hrsh7th/nvim-cmp",
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,8 +1,8 @@
 return {
   {
-   "williamboman/mason.nvim",
+    "williamboman/mason.nvim",
     config = function()
-      local config  = require("mason")
+      local config = require("mason")
       config.setup({
         ui = {
           icons = {
@@ -24,13 +24,75 @@ return {
     end
   },
   {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- FastAPI snippets
+      luasnip.add_snippets("python", {
+        luasnip.snippet("fapi", {
+          luasnip.text_node({"from fastapi import FastAPI", "", "app = FastAPI()", "", ""}),
+          luasnip.text_node({"@app."}), luasnip.insert_node(1, "get"),
+          luasnip.text_node({'("'}), luasnip.insert_node(2, "/"),
+          luasnip.text_node({'")'}), luasnip.text_node({'', 'def '}),
+          luasnip.insert_node(3, "endpoint"),
+          luasnip.text_node({'():', '    return {"message": "'}),
+          luasnip.insert_node(4, "Hello World"),
+          luasnip.text_node({'"}'}),
+        }),
+      })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+    end
+  },
+  {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      -- Server Spec settings
+      
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
@@ -55,19 +117,41 @@ return {
       lspconfig.dockerls.setup({
         capabilities = capabilities
       })
-   -- lspconfig.golangci_lint_ls.setup({
-      --  capabilities = capabilities
-      -- })
       lspconfig.pyright.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
+        on_init = function(client)
+          local venv = vim.fn.findfile('../venv/bin/python', vim.fn.getcwd() .. ';')
+          local poetry = vim.fn.findfile('poetry.lock', vim.fn.getcwd() .. ';')
+          local python_path
+          if venv ~= '' then
+            python_path = vim.fn.fnamemodify(venv, ':p')
+          elseif poetry ~= '' then
+            python_path = vim.fn.trim(vim.fn.system('poetry env info -p')) .. '/bin/python'
+          else
+            python_path = vim.fn.exepath('python3') or vim.fn.exepath('python')
+          end
+          if python_path then
+            client.config.settings.python.pythonPath = python_path
+          end
+        end,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace",
+              typeCheckingMode = "basic",
+            }
+          }
+        }
       })
       lspconfig.bashls.setup({
         capabilities = capabilities
       })
-      --Keybindings for lsp-config
+      
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   },
 }
