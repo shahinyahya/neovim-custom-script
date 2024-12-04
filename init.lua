795116b22e044e6206ac413a1251e19475c4d71b
shahinyahya <shahin@zennode.com>
2024-12-04 11:25:21 +0530

    Add: nul_ls plugin for linting, configure: added default opening for nvim .

--- init.lua
+++ init.lua
@@ -1,17 +1,20 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,6 +25,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 local opts = {}
+
+
+-- Hit enter when entering to nvim to display Neotree
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- To check arg "."
+    local args = vim.fn.argv()
+    if #args == 1 and args[1] == "." then
+      -- Close empyt buffers, if any;
+      vim.cmd("bd")
+      -- Open Neotree in current position
+      vim.cmd("Neotree position=current")
+    end
+  end,
+})
 
 require("vim-keyoptions")
 require("lazy").setup("plugins")
--- lua/vim-keyoptions.lua
+++ lua/vim-keyoptions.lua
@@ -3,3 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+-- Custom Keybindings
+vim.keymap.set({'n', 'v'}, '<TAB>', ':bn<CR>', {silent = true})
+vim.keymap.set({'n', 'v'}, '<S-TAB>', ':bp<CR>', {silent = true})
