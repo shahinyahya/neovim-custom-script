d226371b75088f81e9dbd11def5e799b15d4963b
shahinyahya <shahin@zennode.com>
2024-12-05 13:48:06 +0530

    Add: intentation line plugin

---
+++ lua/plugins/indent-line.lua
@@ -0,0 +1,53 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		local highlight = {
+			"RainbowRed",
+			"RainbowYellow",
+			"RainbowBlue",
+			"RainbowOrange",
+			"RainbowGreen",
+			"RainbowViolet",
+			"RainbowCyan",
+		}
+
+		local hooks = require("ibl.hooks")
+		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+		end)
+
+		require("ibl").setup({
+			indent = {
+				char = "│",
+				highlight = highlight,
+			},
+			scope = {
+				enabled = true,
+				show_start = true,
+				show_end = true,
+				injected_languages = true,
+				highlight = highlight,
+				priority = 500,
+			},
+			exclude = {
+				filetypes = {
+					"help",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"lazy",
+					"mason",
+					"notify",
+				},
+			},
+		})
+	end,
+}
