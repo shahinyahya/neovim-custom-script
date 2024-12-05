09a27c6d8cbc5b3ce95867e5cd0eecd767b52220
shahinyahya <shahin@zennode.com>
2024-12-05 09:38:03 +0530

    minor changes

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -148,7 +148,6 @@ return {
       lspconfig.bashls.setup({
         capabilities = capabilities
       })
-      
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
