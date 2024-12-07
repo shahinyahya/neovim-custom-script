497a3851a1fcfe4d3dd1f458256f727c6ba55e28
shahinyahya <shahintyahya@gmail.com>
2024-12-07 20:37:45 +0530

    Add: new color scheme, modified: null ls configuration changes

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,17 +1,21 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.completion.spell,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.completion.spell,
         null_ls.builtins.formatting.prettier,
         -- null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.diagnostics.mypy,
+        -- null_ls.builtins.formatting.autopep8,
+        null_ls.builtins.formatting.yapf,
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
