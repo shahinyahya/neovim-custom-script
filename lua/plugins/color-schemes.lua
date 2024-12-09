59e8bad0bce64030acbdef2dd65f288101d4c6a4
shahinyahya <shahin@zennode.com>
2024-12-09 14:40:48 +0530

    added color themes (few)

--- lua/plugins/color-schemes.lua
+++ lua/plugins/color-schemes.lua
@@ -1,19 +1,47 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = false,
-  --   name = "catppuccin",
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme "catppuccin"
-  --   end
-  -- },
+	-- {
+	--   "catppuccin/nvim",
+	--   lazy = false,
+	--   name = "catppuccin",
+	--   priority = 1000,
+	--   config = function()
+	--     vim.cmd.colorscheme "catppuccin"
+	--   end
+	-- },
+
+	-- {
+	--   "scottmckendry/cyberdream.nvim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   config = function()
+	--     vim.cmd.colorscheme "cyberdream"
+	--   end
+	-- },
+
+	-- {
+	--   'navarasu/onedark.nvim',
+	--   lazy = false,
+	--   priority = 1000,
+	--   config = function()
+	--     require('onedark').setup({})
+	--   end
+	-- },
+
+	-- {
+	-- 	"morhetz/gruvbox",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("gruvbox")
+	-- 	end,
+	-- },
+
   {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "cyberdream"
-    end
-  },
+    "tiagovla/tokyodark.nvim",
+    opts = {
+        -- custom options here
+    },
+    config = function(_, opts)
+        require("tokyodark").setup(opts) -- calling setup is optional
+        vim.cmd [[colorscheme tokyodark]]
+    end,
+}
 }
